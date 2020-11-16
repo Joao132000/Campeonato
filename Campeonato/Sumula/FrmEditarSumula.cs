980586248bc54d5125c240508a1efc3163fe5d93
Joao Paulo Carvalho <71353908+Joao132000@users.noreply.github.com>
2020-11-15 21:47:04 -0300

    2.3

--- Campeonato/Sumula/FrmEditarSumula.cs
+++ Campeonato/Sumula/FrmEditarSumula.cs
@@ -39,20 +39,20 @@ namespace Campeonato
         private void cmdPesquisar_Click(object sender, EventArgs e)
         {
             dados.AuxNomeJogador = txtNome.Text;
-            dataGridView1.DataSource = dados.ListarDadosJogador().Tables[0];
+            dataGridView1.DataSource = dados.ListarDadosJogadorParaEditar().Tables[0];
         }
 
 
         private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-            id = int.Parse(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
             MessageBox.Show("Dados selecionados!");
             Close();
         }
 
         private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
         {
-           
+            id = int.Parse(dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString());
+
         }
     }
 }
